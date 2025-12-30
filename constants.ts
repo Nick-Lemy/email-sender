39f9d6b59839dd0198bcb7ce0a4a22a5b52956f7
Nick-Lemy <n.kayiranga@alustudent.com>
2025-12-30 20:54:39 +0200

    feat: add email sending functionality with nodemailer
    - Added email configuration constants in constants.ts
    - Implemented email sending logic in sendEmailController in controller.ts
    - Updated package.json to include nodemailer and its type definitions

--- constants.ts
+++ constants.ts
@@ -2,3 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
+export const EMAIL_HOST = process.env.EMAIL_HOST;
+export const EMAIL_USER = process.env.EMAIL_USER;
+export const EMAIL_PASS = process.env.EMAIL_PASS;
+export const EMAIL_PORT = process.env.EMAIL_PORT;
--- controller.ts
+++ controller.ts
@@ -1,9 +1,22 @@
 import type { Request, Response } from "express";
+import { createTransport } from "nodemailer";
+import { EMAIL_HOST, EMAIL_PASS, EMAIL_PORT, EMAIL_USER } from "./constants";
 
 export function sendEmailController(req: Request, res: Response) {
   const { to, subject, body } = req.body;
 
   // TODO: Implement email sending logic here
+  // set up transporter
+  const transporter = createTransport({
+    service: "Gmail",
+    host: EMAIL_HOST,
+    port: Number(EMAIL_PORT),
+    secure: true,
+    auth: {
+      user: EMAIL_USER, // the email you used to create app password
+      pass: EMAIL_PASS, // your generated app password
+    },
+  });
 
   console.log(`Sending email to: ${to}, subject: ${subject}, body: ${body}`);
 
