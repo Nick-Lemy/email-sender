63a0c4ca3767f1b7c7ca7fa35fc90c20fd63d7bc
Nick-Lemy <n.kayiranga@alustudent.com>
2026-01-03 15:40:14 +0200

    feat: add health check endpoint to email service

--- app.route.ts
+++ app.route.ts
@@ -2,5 +2,8 @@ import { Router } from "express";
 import { sendEmailController } from "./app.controller";
 
 const emailRouter = Router();
+emailRouter.get("/health", (req, res) => {
+  res.send("Email service is healthy");
+});
 emailRouter.post("/send", sendEmailController);
 export default emailRouter;
