c23f51a1a48f7e19cd2e663eb90644daed0c94fe
nick-lemy <n.kayiranga@alustudent.com>
2026-01-04 11:49:06 +0000

    feat: add secure header

--- app.ts
+++ app.ts
@@ -9,7 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+       	res.setHeader('Content-Security-Policy', "upgrade-insecure-requests; default-src 'self' http: data:; script-src 'self' http: 'unsafe-inline'; style-src 'self' http: 'unsafe-inline';");
+	next();
+});
 app.get("/health", (req, res) => {
   res.send("OK");
 });
